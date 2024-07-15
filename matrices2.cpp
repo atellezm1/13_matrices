dab3dd511a702014e395ddf2d1ce5c3ea2f5f74e
rhuamanh <rhuamanh@unjbg.edu.pe>
2024-07-15 13:11:14 -0500

    Implementamos un for para la suma de la diagonal secundaria de la matris

--- matrices2.cpp
+++ matrices2.cpp
@@ -2,10 +2,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n,s;
+    int n,s,j;
     s=0;
     cout<<"ingrese el orden de la matris: "<<endl;
     cin>>n;
+    j=n;
     int A[n][n];
     for(int i=1; i<=n;i++){
         for(int j=1; j<=n;j++){
@@ -13,5 +14,12 @@ int main() {
             cin>>A[i][j];
         }
     }
-    
+    for(int i=1; i<=n;i++){
+        s=A[i][j];
+        j=j-1;
+    }
+    cout<<"la sumatoriade la diagonal secundariaes: "<<endl;
+    cout<<s;
+
     return 0;
+}
